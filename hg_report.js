674eeb8af36ab032212ead01ba083ef9521ff61b
Raymond Hernandez <rhernandez028@west-mec.org>
2019-03-01 18:18:32 -0700

    Fixed several bugs
    Fixed several bugs in the second loop in the hg_report.js file. Specifically, the div and the image tag inserted into the HTML. Also added comments

--- hg_report.js
+++ hg_report.js
@@ -17,22 +17,24 @@ var gameReport = "<h1>" + itemTitle + "</h1> <h2>By:" + itemManufacturer + "</h2
 
 // displaying the information
 document.getElementsByTagName('article')[0].innerHTML = gameReport;
+
 //calculate the average customer rating
 var ratingSum = 0;
-var ratingCount = ratings.length;
+var ratingsCount = ratings.length;
+
 
 for (var i = 0; i < ratings.length ; i = ratings + ratingSum){
-    var ratingsAvg = ratingSum[i] / ratingCount[i];
-    var ratingReport = " <h1>Customer Reviews</h1> <h2>" + ratingsAvg + "out of 5 stars (" + ratingCount + " reviews)</h2>";
+    var ratingsAvg = ratingSum / ratingsCount;
+    var ratingReport = " <h1>Customer Reviews</h1> <h2>" + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
 }
 
 // displays first three customer review
 for (var i = 0; i <= 2; i++) {
-    ratingReport += "div class='review'> <h1>" + itemTitle[i] + "</h1> <table> <tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr> <tr><th>Rating</th><td>";
-    for(var j = 1; j <= 4; j++) {
-        ratingReport += "<img src=”hg_star.png” />";
+    ratingReport += "<div class='review'> <h1>" + ratingTitles[i] + "</h1> <table> <tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr> <tr><th>Rating</th><td>";
+    for(var j = 1; j <= 5; j++) {
+        ratingReport += "<img src='hg_star.png' />";
     }
-    ratingReport += "</td></tr></table>" + ratingSummaries + " </div>";
+    ratingReport += "</td></tr></table>" + ratingSummaries[i] + " </div>";
 }
 
 document.getElementsByTagName('aside')[0].innerHTML = ratingReport;
